14c79d4bc9c904b288466bfc14bbf62c7a45ea55
pavlenkovit <vitpvln@gmail.com>
2019-10-04 15:43:55 +0300

    fix

--- components/Pagination/Pagination.jsx
+++ components/Pagination/Pagination.jsx
@@ -3,12 +3,34 @@ import css from './Pagination.module.scss';
 import PageLink from './components/PageLink';
 import Button from './components/Button';
 
+const maxCount = 5;
+
 const Pagination = ({ total, rootAs, rootHref, activePage }) => {
   if (total === 1) {
     return null;
   }
 
-  const pages = new Array(total).fill(0);
+  const getPages = () => {
+    let pages = new Array(total)
+      .fill(0)
+      .map((val, idx) => idx + 1);
+
+    if (total <= maxCount) {
+      return pages;
+    }
+
+    if (activePage > total - maxCount / 2) {
+      return pages.slice(-maxCount);
+    }
+
+    if (activePage < maxCount / 2) {
+      return pages.slice(0, maxCount);
+    }
+
+    return pages.filter(page => page < activePage + maxCount / 2 && page > activePage - maxCount / 2);
+  };
+
+  const pages = getPages();
 
   const hrefPath = (page) => {
     if (rootHref === '/') {
@@ -35,18 +57,21 @@ const Pagination = ({ total, rootAs, rootHref, activePage }) => {
         </Button>
       )}
       <div className={css.list}>
-        {pages.map((val, idx) => {
-          const page = idx + 1;
-          return (
-            <PageLink
-              href={hrefPath(page)}
-              as={asPath(page)}
-              isActive={activePage === page}
-            >
-              {page}
-            </PageLink>
-          );
-        })}
+        {pages[0] !== 1 && (
+          <div className={css.dots}>...</div>
+        )}
+        {pages.map((page) => (
+          <PageLink
+            href={hrefPath(page)}
+            as={asPath(page)}
+            isActive={activePage === page}
+          >
+            {page}
+          </PageLink>
+        ))}
+        {pages[pages.length - 1] !== total && (
+          <div className={css.dots}>...</div>
+        )}
       </div>
       {activePage < total && (
         <Button
