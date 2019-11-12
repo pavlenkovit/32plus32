179ee1b34b946737641338e9f284d5ff4a6d0776
pavlenkovit <vitpvln@gmail.com>
2019-11-12 22:29:59 +0300

    fix

---
+++ Slide/Slide.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './Slide.module.scss';
+
+const Slide = () => {
+  return (
+    <div className={css.container}>
+    </div>
+  );
+};
+
+export default Slide;
---
+++ StickySlider/StickySlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './StickySlider.module.scss';
+
+const StickySlider = () => {
+  return (
+    <div className={css.container}>
+    </div>
+  );
+};
+
+export default StickySlider;
