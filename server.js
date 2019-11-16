bb2760dfd7153b29a8916fb470009e6ea1979108
pavlenkovit <vitpvln@gmail.com>
2019-11-16 03:52:56 +0300

    f

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ const port = 3001;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const redirects = [
+  { from: '/xvost-drakona-uprazhnenie-s-girej-na-press', to: '/post/xvost-drakona-uprazhnenie-s-girej-na-press' },
+];
+
 app.prepare()
   .then(() => {
     const server = express();
+
+    redirects.forEach(({ from, to, type = 301, method = 'get' }) => {
+      server[method](from, (req, res) => {
+        res.redirect(type, to);
+      })
+    });
 
     server.get('*', (req, res) => {
       return handle(req, res)
