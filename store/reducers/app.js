1d116debc8043fec28e42b908cb943115146d1c4
pavlenkovit <vitpvln@gmail.com>
2019-11-18 15:41:33 +0300

    fix

--- store/reducers/app.js
+++ store/reducers/app.js
@@ -2,7 +2,7 @@ import { handleActions } from 'redux-actions';
 import * as actions from '../actions';
 
 const initial = {
-  isMobile: false,
+  isMobile: true,
   menuIsActive: false,
   searchIsActive: false,
 };
