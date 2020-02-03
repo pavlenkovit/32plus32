6d2beafd31fbf96d357f380d54a9a3e486521fd5
pavlenkovit <vitpvln@gmail.com>
2020-02-03 14:15:04 +0300

    fix

---
+++ cli/test.js
@@ -0,0 +1,147 @@
+const str = `<h2>Введение. Важность правильной техники в гиревом спорте</h2>
+
+
+
+<p>Одним из решающих факторов достижения высоких спортивных результатов в гиревом спорте является овладение рациональной спортивной техникой, что в свою очередь дает возможность гиревику использовать свои физические качества и тем самым достигать высоких результатов.</p>
+
+
+
+<p>Различные нарушения правильной техники толчка и рывка задерживают рост достижений спортсмена, ограничивают его возможности.</p>
+
+
+
+<p>Соревновательные упражнения с гирями отличаются достаточной сложностью координации мышечных напряжений. Выполнение этих упражнений связано с предельным напряжением мышц и быстрыми сменами режима работы &#8212; напряжения и расслабления; гиревик к тому же должен сохранять равновесие во всех опорных фазах движений.</p>
+
+
+
+<p>Надежность и экономичность в исполнении соревновательных упражнений гиревого спорта достигается кропотливой работой по совершенствованию технического мастерства, обеспечивающей прочную автоматизацию движений и устойчивость реализации двигательных навыков по отношению к различным, как внутренним, так и внешним сбивающим факторам.</p>
+
+
+
+<p>В этой статье будет дано краткое описание <strong>техники выполнения толчка 2-х гирь от груди</strong>.</p>
+
+
+
+<h2>Основные элементы техники толчка гирь</h2>
+
+
+
+<p>В толчке можно выделить следующие технические элементы: старт, подъем на грудь, исходное положение (далее ИП) перед выталкиванием (основной старт), первый подсед, выталкивание, второй подсед, выход из второго подседа, фиксация, опускание на грудь.</p>
+
+
+
+<div class="frame-wrapper-rect"><figure><iframe src="https://www.youtube.com/embed/KliO_hlUYw0" frameborder="0" allowfullscreen="allowfullscreen"></iframe></figure></div>
+
+
+
+<h3>Старт</h3>
+
+
+
+<p>Согнутые в коленях ноги немного шире, гири стоят впереди в 15-20 см от линии носков ног, руки прямые,&nbsp; захват произвольный (как правило сверху или нейтральный), спина прямая, голова в нейтральном положении (<strong>Рис. 1</strong>).</p>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/1.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/1.jpg" alt="Толчок гирь. Старт" class="wp-image-974" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/1.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/1-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/1-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/1-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 1. Старт</figcaption></figure>
+
+
+
+<h3>Подъем гирь на грудь</h3>
+
+
+
+<p>Из стартового положения, оторвав гири от помоста, сделать замах между ног назад (<strong>Рис. 2</strong>). Затем за счет резкого выпрямления ног и спины выполнить подрыв (<strong>Рис. 3</strong>), и, согнув ноги в коленях, &#8212; подсед (<strong>Рис. 4</strong>). Забросить гири на плечи, после чего выпрямить ноги.</p>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/2.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/2.jpg" alt="Замах гирь назад" class="wp-image-975" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/2.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/2-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/2-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/2-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 2. Замах гирь назад</figcaption></figure>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/3.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/3.jpg" alt="Подрыв" class="wp-image-976" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/3.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/3-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/3-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/3-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 3. Подрыв</figcaption></figure>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/4.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/4.jpg" alt="Подсед после заброса" class="wp-image-977" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/4.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/4-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/4-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/4-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 4. Подсед после заброса</figcaption></figure>
+
+
+
+<p>Первые два элемента толчка выполняются только один раз после команды «старт». Все остальные повторения начинаются из положения основного старта.</p>
+
+
+
+<h3>Основной старт (ИП перед выта
+Post.tsx:20 
+<h2>Введение. Важность правильной техники в гиревом спорте</h2>
+
+
+
+<p>Одним из решающих факторов достижения высоких спортивных результатов в гиревом спорте является овладение рациональной спортивной техникой, что в свою очередь дает возможность гиревику использовать свои физические качества и тем самым достигать высоких результатов.</p>
+
+
+
+<p>Различные нарушения правильной техники толчка и рывка задерживают рост достижений спортсмена, ограничивают его возможности.</p>
+
+
+
+<p>Соревновательные упражнения с гирями отличаются достаточной сложностью координации мышечных напряжений. Выполнение этих упражнений связано с предельным напряжением мышц и быстрыми сменами режима работы &#8212; напряжения и расслабления; гиревик к тому же должен сохранять равновесие во всех опорных фазах движений.</p>
+
+
+
+<p>Надежность и экономичность в исполнении соревновательных упражнений гиревого спорта достигается кропотливой работой по совершенствованию технического мастерства, обеспечивающей прочную автоматизацию движений и устойчивость реализации двигательных навыков по отношению к различным, как внутренним, так и внешним сбивающим факторам.</p>
+
+
+
+<p>В этой статье будет дано краткое описание <strong>техники выполнения толчка 2-х гирь от груди</strong>.</p>
+
+
+
+<h2>Основные элементы техники толчка гирь</h2>
+
+
+
+<p>В толчке можно выделить следующие технические элементы: старт, подъем на грудь, исходное положение (далее ИП) перед выталкиванием (основной старт), первый подсед, выталкивание, второй подсед, выход из второго подседа, фиксация, опускание на грудь.</p>
+
+
+
+<div class="frame-wrapper-rect"><figure><iframe src="https://www.youtube.com/embed/KliO_hlUYw0" frameborder="0" allowfullscreen="allowfullscreen"></iframe></figure></div>
+
+
+
+<h3>Старт</h3>
+
+
+
+<p>Согнутые в коленях ноги немного шире, гири стоят впереди в 15-20 см от линии носков ног, руки прямые,&nbsp; захват произвольный (как правило сверху или нейтральный), спина прямая, голова в нейтральном положении (<strong>Рис. 1</strong>).</p>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/1.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/1.jpg" alt="Толчок гирь. Старт" class="wp-image-974" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/1.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/1-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/1-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/1-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 1. Старт</figcaption></figure>
+
+
+
+<h3>Подъем гирь на грудь</h3>
+
+
+
+<p>Из стартового положения, оторвав гири от помоста, сделать замах между ног назад (<strong>Рис. 2</strong>). Затем за счет резкого выпрямления ног и спины выполнить подрыв (<strong>Рис. 3</strong>), и, согнув ноги в коленях, &#8212; подсед (<strong>Рис. 4</strong>). Забросить гири на плечи, после чего выпрямить ноги.</p>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/2.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/2.jpg" alt="Замах гирь назад" class="wp-image-975" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/2.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/2-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/2-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/2-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 2. Замах гирь назад</figcaption></figure>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/3.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/3.jpg" alt="Подрыв" class="wp-image-976" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/3.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/3-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/3-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/3-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 3. Подрыв</figcaption></figure>
+
+
+
+<figure class="wp-block-image alignnone"><a href="https://api.32plus32.ru/wp-content/uploads/2018/12/4.jpg"><img src="https://api.32plus32.ru/wp-content/uploads/2018/12/4.jpg" alt="Подсед после заброса" class="wp-image-977" srcset="https://api.32plus32.ru/wp-content/uploads/2018/12/4.jpg 1200w, https://api.32plus32.ru/wp-content/uploads/2018/12/4-300x200.jpg 300w, https://api.32plus32.ru/wp-content/uploads/2018/12/4-768x512.jpg 768w, https://api.32plus32.ru/wp-content/uploads/2018/12/4-1024x683.jpg 1024w" sizes="(max-width: 1200px) 100vw, 1200px" /></a><figcaption>Рис. 4. Подсед после заброса</figcaption></figure>
+
+<div class="frame-wrapper-landscape"><figure><iframe src="//vk.com/video_ext.php?oid=-52965338&amp;id=456239046&amp;hash=43b5e4f00a3140df&amp;hd=2" frameborder="0" allowfullscreen="allowfullscreen"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span></iframe></figure></div>
+
+<p>Первые два элемента толчка выполняются только один раз после команды «старт». Все остальные повторения начинаются из положения основного старта.</p>`;
+
+const matches = str.match(/\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/ig);
+
+console.log(matches);
